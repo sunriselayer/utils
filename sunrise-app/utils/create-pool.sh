43658c18ef88495e25c38a912328618303168d7c
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-07-08 02:32:37 +0000

    feat: pool & position

--- sunrise-app/utils/create-pool.sh
+++ sunrise-app/utils/create-pool.sh
@@ -3,54 +3,55 @@
 # ATOM/RISE
 sunrised tx liquiditypool create-pool --denom-base=ibc/uatom --denom-quote=urise \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # RISE/USDC
 sunrised tx liquiditypool create-pool --denom-base=urise --denom-quote=ibc/uusdc \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # RISE/USDT
 sunrised tx liquiditypool create-pool --denom-base=urise --denom-quote=ibc/uusdt \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # USDT/USDC
 sunrised tx liquiditypool create-pool --denom-base=ibc/uusdt --denom-quote=ibc/uusdc \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # SHIB.axl/USDC
 sunrised tx liquiditypool create-pool --denom-base=ibc/ushib --denom-quote=ibc/uusdc \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
+sleep 30
 
 # iBGT/USDC
 sunrised tx liquiditypool create-pool --denom-base=ibc/uibgt --denom-quote=ibc/uusdc \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # stATOM/ATOM
 sunrised tx liquiditypool create-pool --denom-base=ibc/stuatom --denom-quote=ibc/uatom \
 --fee-rate="0.001" --price-ratio="1.000100000000000000" --base-offset="0.500000000000000000" \
---from=my_validator --chain-id=sunrise-test \
+--from=my_validator --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # Gauge Vote
 sunrised tx liquidityincentive vote-gauge --weights='{"pool_id":0,"weight":"100000000000000000"}' --weights='{"pool_id":1,"weight":"100000000000000000"}' \
 --weights='{"pool_id":2,"weight":"100000000000000000"}' --weights='{"pool_id":3,"weight":"100000000000000000"}' \
 --weights='{"pool_id":4,"weight":"100000000000000000"}' --weights='{"pool_id":5,"weight":"100000000000000000"}' \
 --weights='{"pool_id":6,"weight":"100000000000000000"}'  --from=my_validator \
---chain-id=sunrise-test --yes --keyring-backend=test --fees="10000urise"
+--chain-id=sunrise-test-0.1 --yes --keyring-backend=test --fees="10000urise"
--- sunrise-app/utils/create-position.sh
+++ sunrise-app/utils/create-position.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 
 # ATOM/RISE
-sunrised tx liquiditypool create-position --pool-id=0 \
+sunrised tx liquiditypool create-position --pool-id=1 \
 --lower-tick="-8210" --upper-tick="-100" \
---token-base=5000000000000ibc/uatom --token-quote=3300000000000urise \
+--token-base=500000000000ibc/uatom --token-quote=330000000000urise \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user1 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # RISE/USDC
-sunrised tx liquiditypool create-position --pool-id=1 \
+sunrised tx liquiditypool create-position --pool-id=2 \
 --lower-tick=18972 --upper-tick=27081 \
 --token-base=800000000000urise --token-quote=8000000000000ibc/uusdc \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user2 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # RISE/USDT
-sunrised tx liquiditypool create-position --pool-id=2 \
+sunrised tx liquiditypool create-position --pool-id=3 \
 --lower-tick=18972 --upper-tick=27081 \
---token-base=800000000000urise --token-quote=8000000000000ibc/uusdt \
+--token-base=100000000000urise --token-quote=1000000000000ibc/uusdt \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user2 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # USDT/USDC
-sunrised tx liquiditypool create-position --pool-id=3 \
+sunrised tx liquiditypool create-position --pool-id=4 \
 --lower-tick="-4155" --upper-tick=4054 \
 --token-base=50000000000000ibc/uusdt --token-quote=50000000000000ibc/uusdc \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user2 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # SHIB.axl/USDC
-sunrised tx liquiditypool create-position --pool-id=4 \
+sunrised tx liquiditypool create-position --pool-id=5 \
 --lower-tick="-114489" --upper-tick="-106379" \
 --token-base=100000000000000000ibc/ushib --token-quote=1600000000000ibc/uusdc \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user2 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # iBGT/USDC
-sunrised tx liquiditypool create-position --pool-id=5 \
+sunrised tx liquiditypool create-position --pool-id=6 \
 --lower-tick=20795 --upper-tick=25258 \
 --token-base=200000000000ibc/uibgt --token-quote=2000000000000ibc/uusdc \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user2 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
 
 # stATOM/ATOM
-sunrised tx liquiditypool create-position --pool-id=6 \
+sunrised tx liquiditypool create-position --pool-id=7 \
 --lower-tick="-4155" --upper-tick=4054 \
 --token-base=5000000000000ibc/stuatom --token-quote=5000000000000ibc/uatom \
 --min-amount-base="0" --min-amount-quote="0" \
---from=user2 --chain-id=sunrise-test \
+--from=user2 --chain-id=sunrise-test-0.1 \
 --yes --keyring-backend=test --fees="10000urise"
-sleep 12
+sleep 20
