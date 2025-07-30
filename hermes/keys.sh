72f57d80c537d1d324e67dac6a8b3d6b50e07583
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-07-30 16:14:49 +0000

    feat: pion

--- hermes/keys.sh
+++ hermes/keys.sh
@@ -3,3 +3,4 @@
 hermes keys add --key-name user1 --mnemonic-file user1.txt  --chain provider
 hermes keys add --key-name user1 --mnemonic-file user1.txt  --chain grand-1
 hermes keys add --key-name user1 --mnemonic-file user1.txt  --chain dawn-1
+hermes keys add --key-name user1 --mnemonic-file user1.txt  --chain pion-1
